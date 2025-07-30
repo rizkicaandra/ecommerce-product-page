e2a515f4173baec632ff30f264e1ef2105bb3bf4
mhd rizki candra <rizki.candra@ifortepay.id>
2025-07-30 20:36:03 +0700

    initiation

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Ecommerce Product Page',
+  description: 'Frontend Mentor Challenge',
+  icons: '/favicon-32x32.png',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='anonymous'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@100..900&display=swap'
+          rel='stylesheet'
+        ></link>
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
